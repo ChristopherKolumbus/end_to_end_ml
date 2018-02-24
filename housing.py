82796004852a46fcb4fcaec6faebfba9aece753e
christoph <chris24.blessing@gmail.com>
2018-02-24 13:32:04 +0100

    Added function to split train and test data

--- housing.py
+++ housing.py
@@ -3,6 +3,7 @@ import shutil
 import tarfile
 
 import requests
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -13,8 +14,8 @@ def main():
     housing_url = download_root + housing_path + "/housing.tgz"
     fetch_housing_data(housing_url, housing_path)
     housing = load_housing_data(housing_path)
-    housing.hist(bins=50, figsize=(20, 15))
-    plt.show()  
+    train_set, test_set = split_train_test(housing, .2)
+    print(len(test_set), len(train_set))
 
 
 def fetch_housing_data(housing_url, housing_path):
@@ -33,6 +34,14 @@ def fetch_housing_data(housing_url, housing_path):
 def load_housing_data(housing_path):
     csv_path = os.path.join(housing_path, 'housing.csv')
     return pd.read_csv(csv_path)
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
 
 
 if __name__ == '__main__':
